280100bef71e9473055aad01d38803d3e3fc0999
Zac Colley <hi@zaccolley.com>
2015-11-30 23:23:00 +0000

    Added a sending screen to the notification process

--- src/containers/App/Front/FrontInstructionReception/FrontInstructionReception.js
+++ src/containers/App/Front/FrontInstructionReception/FrontInstructionReception.js
@@ -4,12 +4,26 @@ import { Icon } from 'components';
 
 export default class FrontInstructionReception extends Component {
 
+  state = {
+    sending: true
+  };
+
+  componentDidMount() {
+    // after 5 seconds the message should have been sent
+    setTimeout(() => {
+      this.setState({ sending: false });
+    }, 3000);
+  }
+
   render() {
+
+    const { sending } = this.state;
+
     return (
       <main className="page page--instruction-reception">
       <div className="container">
 
-        <div className="top-nav">
+        <div className="top-nav" style={{ visibility: (sending ? 'hidden' : 'visible') }}>
           <Link className="back-button" to="/front">
             <Icon name="chevron-left" /> Home
           </Link>
@@ -17,9 +31,18 @@ export default class FrontInstructionReception extends Component {
         </div>
 
         <div className="instruction-message">
-          <Icon name="thumbs-up" large />
-          <p><em>They have been notified!</em></p>
-          <p>Please wait in <strong>the reception</strong>&hellip;</p>
+          {sending ? (
+          <div>
+            <Icon name="sync" large spin />
+            <p><em>Sending them a notification...</em></p>
+          </div>
+          ) : (
+          <div>
+            <Icon name="thumbs-up" large />
+            <p><em>They have been notified!</em></p>
+            <p>Please wait in <strong>the reception</strong>&hellip;</p>
+          </div>
+          )}
         </div>
 
       </div>
