d78c785d6d874d498a1dc93c81e870d8bf182051
Zac Colley <hi@zaccolley.com>
2015-12-01 09:10:59 +0000

    10 second timeout on reception page

--- src/containers/App/Front/FrontInstructionReception/FrontInstructionReception.js
+++ src/containers/App/Front/FrontInstructionReception/FrontInstructionReception.js
@@ -1,18 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import { Icon } from 'components';
+import { pushState } from 'redux-router';
+import { connect } from 'react-redux';
 
+@connect(
+  state => ({
+    error: state.notifications.error,
+  }),
+  { pushState })
 export default class FrontInstructionReception extends Component {
+  static propTypes = {
+    pushState: PropTypes.func.isRequired
+  }
 
   state = {
     sending: true
   };
 
   componentDidMount() {
-    // after 5 seconds the message should have been sent
-    setTimeout(() => {
+
+    // after 3 seconds the message should have been sent
+    this.sendingTimer = setTimeout(() => {
       this.setState({ sending: false });
     }, 3000);
+
+    // after 10 seconds the page should go back to the home
+    this.timeoutTimer = setTimeout(() => {
+      this.props.pushState(null, '/front'); // redirect to homepage
+    }, 13000);
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.sendingTimer);
+    clearInterval(this.timeoutTimer);
   }
 
   render() {
